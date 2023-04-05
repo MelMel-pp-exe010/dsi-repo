73d8a2b8b52e118abfd38afcddfd1dfeaf7e31fe
Melissa <melissa.araujo.map@gmail.com>
2023-04-05 20:52:00 -0300

    Tópico 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,14 +12,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: RandomWords(),
-        ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.white,
+            foregroundColor: Color.fromARGB(255, 241, 113, 141)),
       ),
+      home: const RandomWords(),
     );
   }
 }
@@ -28,27 +27,134 @@ class RandomWords extends StatefulWidget {
   const RandomWords({Key? key}) : super(key: key);
 
   @override
-  State<RandomWords> createState() => _RandomWordsState();
+  _RandomWordsState createState() => _RandomWordsState();
 }
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18);
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _saved = <WordPair>[];
+  bool cardModen = false;
+
+  get child => null;
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if(i.isOdd) return const Divider();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup Name Generator'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.list),
+              onPressed: _pushSaved,
+              tooltip: 'Saved Suggestions',
+            ),
+            IconButton(
+              onPressed: (() {
+                setState(() {
+                  if (cardModen == false) {
+                    cardModen = true;
+                  } else if (cardModen == true) {
+                    cardModen = false;
+                  }
+                });
+              }),
+              tooltip:
+                  cardModen ? 'List Vizualization' : 'Card Mode Vizualization',
+              icon: Icon(Icons.auto_fix_normal_outlined),
+            ),
+          ],
+        ),
+        body: _buildSuggestions(cardModen));
+  }
 
-        final index = i ~/2;
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(builder: (BuildContext context) {
+        final pianotiles = _saved.map(
+          (WordPair pair) {
+            return ListTile(
+              title: Text(
+                pair.asPascalCase,
+                style: _biggerFont,
+              ),
+            );
+          },
+        );
+        final divided = pianotiles.isNotEmpty
+            ? ListTile.divideTiles(
+                context: context,
+                tiles: pianotiles,
+              ).toList()
+            : <Widget>[];
+
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Favoritas'),
+          ),
+          body: ListView(children: divided),
+        );
+      }),
+    );
+  }
+
+  Widget _buildSuggestions(bool cardMode) {
+    if (cardMode == false) {
+      return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return const Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildList(_suggestions[index], index);
+        },
+      );
+    } else {
+      return _cardList();
+    }
+  }
+
+  Widget _buildList(WordPair pair, int index) {
+    final alreadySaved = _saved.contains(_suggestions[
+        index]); 
+    return ListTile(
+        title: Text(
+          _suggestions[index].asPascalCase,
+          style: _biggerFont,
+        ),
+        trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Color.fromARGB(255, 37, 94, 117) : null,
+            semanticLabel: alreadySaved ? 'Remover dos favoritos' : 'Favoritar'),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _saved.remove(_suggestions[index]);
+            } else {
+              _saved.add(_suggestions[index]);
+            }
+          });
+        });
+  }
+
+  Widget _cardList() {
+    return GridView.builder(
+      padding: const EdgeInsets.all(12),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 2,
+          mainAxisSpacing: 2,
+          childAspectRatio: 8),
+      itemCount: _suggestions.length,
+      itemBuilder: (context, index) {
+        //final index = i ~/ 2;
         if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
-        return ListTile(title: Text(
-            _suggestions[index].asPascalCase,
-            style: _biggerFont,
-          ),
+        return Column(
+          children: [_buildList(_suggestions[index], index)],
         );
       },
     );
