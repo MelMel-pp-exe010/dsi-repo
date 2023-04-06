9c8afa4a7aaf0b8ee09fcaac3f7da0dac3d2dbab
Melissa <melissa.araujo.map@gmail.com>
2023-04-05 22:26:25 -0300

    Tópico 4

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,10 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.white,
             foregroundColor: Color.fromARGB(255, 241, 113, 141)),
       ),
-      home: const RandomWords(),
+      routes: {
+        '/': (context) => const RandomWords(),
+        '/editar':(context) => const EditScreen()
+      }
     );
   }
 }
@@ -31,7 +34,7 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
+  final WordRepository _suggestions = WordRepository();
   final _biggerFont = const TextStyle(fontSize: 18.0);
   final _saved = <WordPair>[];
   bool cardModen = false;
@@ -99,6 +102,9 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildSuggestions(bool cardMode) {
+
+    final WordRepository suggestions;
+
     if (cardMode == false) {
       return ListView.builder(
         padding: const EdgeInsets.all(16.0),
@@ -106,10 +112,8 @@ class _RandomWordsState extends State<RandomWords> {
           if (i.isOdd) return const Divider();
 
           final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildList(_suggestions[index], index);
+          
+          return _buildList(_suggestions.words[index], index);
         },
       );
     } else {
@@ -118,25 +122,47 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildList(WordPair pair, int index) {
-    final alreadySaved = _saved.contains(_suggestions[
+    final alreadySaved = _saved.contains(_suggestions.words[
         index]); 
     return ListTile(
         title: Text(
-          _suggestions[index].asPascalCase,
+          _suggestions._words[index].asPascalCase,
           style: _biggerFont,
         ),
-        trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
-            color: alreadySaved ? Color.fromARGB(255, 37, 94, 117) : null,
-            semanticLabel: alreadySaved ? 'Remover dos favoritos' : 'Favoritar'),
-        onTap: () {
-          setState(() {
-            if (alreadySaved) {
-              _saved.remove(_suggestions[index]);
-            } else {
-              _saved.add(_suggestions[index]);
-            }
-          });
-        });
+        trailing: Column(
+          children: [
+            GestureDetector(
+              onTap: () {
+              setState(() {
+                if (alreadySaved) {
+                  _saved.remove(_suggestions._words[index]);
+                } else {
+                  _saved.add(_suggestions._words[index]);
+                }
+              });
+            },
+              child: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+                  color: alreadySaved ? Color.fromARGB(255, 37, 94, 117) : null,
+                  semanticLabel: alreadySaved ? 'Remover dos favoritos' : 'Favoritar'),
+            ),
+          
+          GestureDetector(
+                  onTap: () async {
+                    final updatedWord = await Navigator.pushReplacementNamed(context, '/editar', arguments: {'word': _suggestions.words[index]});
+                    setState(() {
+                      _suggestions.words[index] = updatedWord;
+                    });
+                  },
+                  child: Icon(
+                    Icons.edit,
+                    color: alreadySaved ? Colors.red : null,
+                    semanticLabel: alreadySaved ? "Remove from saved" : "Save",
+                  ),
+                ),
+
+          ],
+        ),
+        );
   }
 
   Widget _cardList() {
@@ -147,16 +173,63 @@ class _RandomWordsState extends State<RandomWords> {
           crossAxisSpacing: 2,
           mainAxisSpacing: 2,
           childAspectRatio: 8),
-      itemCount: _suggestions.length,
+      itemCount: _suggestions.words.length,
       itemBuilder: (context, index) {
         //final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
         return Column(
-          children: [_buildList(_suggestions[index], index)],
+          children: [_buildList(_suggestions.words[index], index)],
         );
       },
     );
   }
 }
+
+class WordRepository {
+  List<WordPair> _words = generateWordPairs().take(20).toList();
+
+  get words => _words;
+  set setWords (List<WordPair> newList) {
+    _words = newList;
+  }
+}
+
+class EditScreen extends StatefulWidget {
+  const EditScreen({super.key});
+
+  @override
+  State<EditScreen> createState() => _EditScreenState();
+}
+
+class _EditScreenState extends State<EditScreen> {
+  @override
+  Widget build(BuildContext context) {
+final args = ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+    final word = args['word'];
+    String? newWord = '';
+    final controller = TextEditingController(text: word);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Editar palavra"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            TextFormField(
+              controller: controller,
+              onSaved: (value) {
+                newWord = value;
+              },
+            ),
+            ElevatedButton(onPressed: () {
+              final form = Form.of(context);
+              if(form.validate()){
+                form.save();
+                Navigator.pop(context, newWord);
+              }
+            }, child: const Text('Confirmar'))
+          ],
+        )
+      ),
+    );  }
+}
+
